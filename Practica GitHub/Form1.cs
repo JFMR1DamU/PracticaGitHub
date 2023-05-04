44258845e5880ede8bafe95fe8911660de189b74
Dam1uJFMR <juanfrancisco.mramos@gmail.com>
2023-05-04 07:34:45 +0200

    Corregido erroes de la practica

--- Practica GitHub/Form1.cs
+++ Practica GitHub/Form1.cs
@@ -22,7 +22,7 @@ namespace Practica_GitHub
             string textoTelegrama;
             char tipoTelegrama = ' ';
             int numPalabras = 0;
-            double coste;
+            double coste = 0;
             //Leo el telegrama
             textoTelegrama = txtTelegrama.Text;
             // telegrama urgente?
@@ -31,20 +31,22 @@ namespace Practica_GitHub
             //Obtengo el número de palabras que forma el telegrama
             numPalabras = textoTelegrama.Length;
             //Si el telegrama es ordinario
+            if (!cbUrgente.Checked)
+                tipoTelegrama = 'o';
+
             if (tipoTelegrama == 'o')
                 if (numPalabras <= 10)
                     coste = 25;
                 else
                     coste = 0.5 * numPalabras;
-            else
+
             //Si el telegrama es urgente
             if (tipoTelegrama == 'u')
                 if (numPalabras <= 10)
                     coste = 5;
                 else
                     coste = 5 + 0.75 * (numPalabras - 10);
-            else
-                coste = 0;
+
             txtPrecio.Text = coste.ToString() + " euros";
         }
     }
